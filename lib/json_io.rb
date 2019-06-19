e3293ec8319d050b89909f9c04c33060c35811e6
s4na <appletea.umauma@gmail.com>
2019-06-20 03:36:46 +0900

    微細なバグ修正：json_io

--- lib/json_io.rb
+++ lib/json_io.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
+require "json"
+
 module Json
+
   class Io
+    attr_reader :json_data
+
     def initialize(filename)
       @file_path = filename
     end
 
+    # def json_data
+    #   @json_data
+    # end
+
     def read
-      @json_data = open(@file_path) do |io|
-        JSON.load(io)
+      if ::File.exist?(@file_path)
+        @initial_json_data = open(@file_path, "r") do |io|
+          ::JSON.load(io)
+        end
       end
+      @json_data = @initial_json_data
     end
 
     def update(data)
@@ -19,43 +31,44 @@ module Json
       data.each do |key, value|
         @json_data[":#{key}"] = value
       end
-    end
-
-    def show
-      p @json_data
+      save
     end
 
     def delete
       @json_data = {}
+      save
     end
 
-    def save
-      if @json_data != {}
-        open(@file_path, "w+") do |io|
-          JSON.dump(@json_data, io)
+    private
+
+      def save
+        # 問題点: 修正中に他のトランザクションで修正が行われたらデータが保護されない。上書きされる。
+
+        if @json_data != {}
+          open(@file_path, "w") do |io|
+            ::JSON.dump(@json_data, io)
+          end
         end
+        read
       end
-    end
   end
 end
 
-fi = Json::Io.new("../data/1.json")
+fi = Json::Io.new("../data/2.json")
 p "read"
 fi.read
 
 p "show"
-fi.show
+p fi.json_data
 
 p "update"
 fi.update(name: "aaa", content: "iii")
 
 p "show"
-fi.show
-# p "delete"
-# fi.delete
+p fi.json_data
+
+p "delete"
+fi.delete
 
 p "show"
-fi.show
-
-p "save"
-fi.save
+p fi.json_data
