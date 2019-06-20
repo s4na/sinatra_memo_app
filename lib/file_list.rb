73ced5f1940156b77c49c785cf87742733b07bcd
s4na <appletea.umauma@gmail.com>
2019-06-20 18:24:39 +0900

    debug文削除

--- lib/file_list.rb
+++ lib/file_list.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class FileList
-  attr_reader :list
+  attr_reader :list, :only_file_names
 
   def initialize
     @list = []
+    @only_file_names = []
   end
 
   def make(path)
@@ -12,12 +13,14 @@ class FileList
     # 作成：pathの`/*`消す処理
     # 課題：絶対パスにする必要あるかも？？
     @list = Dir.glob("#{path}/*")
-  end
 
-  # debug def
-  # def show
-  #   p @list
-  # end
+    @list.each do |file_path|
+      @only_file_names.push(File.basename(file_path, ".json"))
+    end
+
+    p "@only_file_names = #{@only_file_names}" # debug
+    @list
+  end
 end
 
 # fl = FileList.new
